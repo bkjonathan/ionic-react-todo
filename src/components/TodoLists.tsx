0a87bae84dfb6320f922fc782a06349e08850a6e
bkjonathan <kyawhtetsan.jar1990@gmail.com>
2024-07-20 13:30:42 +0700

    Update list

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -1,16 +1,50 @@
 import { FC } from "react";
 import { useTodo } from "../contexts/TodoContext";
-import { IonItem, IonLabel, IonList } from "@ionic/react";
+import {
+  IonCheckbox,
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+} from "@ionic/react";
+import { pencil, trash } from "ionicons/icons";
+import "./TodoLists.css";
 
 const TodoLists: FC = () => {
-  const { todos } = useTodo(); // Assuming useTodo returns an object with a todos array
+  const { todos, completeTodo } = useTodo(); // Assuming useTodo returns an object with a todos array
 
+  const toggleTodoComplete = (id: number) => {
+    // Assuming you have a function to toggle the todo complete status
+    completeTodo(id);
+  };
   return (
     <IonList>
       {todos.map((todo) => (
-        <IonItem key={todo.id}>
-          <IonLabel>{todo.title}</IonLabel>
-        </IonItem>
+        <IonItemSliding key={todo.id}>
+          <IonItem>
+            <IonCheckbox
+              slot="start"
+              checked={todo.completed}
+              onIonChange={() => toggleTodoComplete(todo.id)}
+            />
+            <IonLabel className={todo.completed ? "todo-completed" : ""}>
+              {todo.title} {todo.completed}
+            </IonLabel>
+          </IonItem>
+          <IonItemOptions>
+            <IonItemOption color="danger">
+              <IonIcon icon={trash} />
+            </IonItemOption>
+          </IonItemOptions>
+          <IonItemOptions side="start">
+            <IonItemOption>
+              <IonIcon icon={pencil} />
+            </IonItemOption>
+          </IonItemOptions>
+        </IonItemSliding>
       ))}
     </IonList>
   );
